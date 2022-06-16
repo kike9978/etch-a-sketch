3c1a8efab80465e0023826a51a23c52b68fc28fc
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-06-16 15:12:59 -0500

    Sets giveRandomColor function to give full range rgb random color

--- scripts/script.js
+++ scripts/script.js
@@ -11,12 +11,14 @@ for(let i = 0; i<16; i++){
 const tiles = document.querySelectorAll(".tile");
 tiles.forEach(tile => tile.addEventListener("click", cambiaColor))
 
-function giveRandomColor(){
-    randomColor = Math.floor(Math.random() * 255)
-    return randomColor;
-
-}
 
 function cambiaColor(e){
-    e.target.style.backgroundColor = `#${giveRandomColor()}`;
+    e.target.style.backgroundColor = `rgb(${giveRandomColor()}, 
+    ${giveRandomColor()}, ${giveRandomColor()})`;
+    console.log(e.target.style.backgroundColor);
+}
+
+function giveRandomColor(){
+    const randomColor = Math.floor(Math.random() * 255);
+    return randomColor;
 }
