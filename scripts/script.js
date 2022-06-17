634ce111f3f442a93691e27f2b1beea979ea5a69
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-06-17 13:02:04 -0500

    Resizes grid tiles to the side varibale

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,30 @@ const container = document.querySelector(".container");
 const div = document.createElement("div");
 
 
-for(let i = 0; i<16; i++){
+// Create a modular flex square grid that can change the cuantity of 
+// elements in the grid without changing the size of its container
+
+let lado = 100;
+let tamañoDeGrid = lado * lado;
+
+
+for(let i = 0; i<tamañoDeGrid; i++){
     const div = document.createElement("div");
     div.classList = "tile";
-    container.appendChild(div);    
+    container.appendChild(div);
 }
 
 
 const tiles = document.querySelectorAll(".tile");
 tiles.forEach((tile) => tile.addEventListener("mouseover", cambiaColor))
 
-
+function cambiaTamanoDeGrid(){
+    console.log(lado);
+    console.log(100/lado);
+    let basis = `${Math.round(100/lado)}%`;
+    tiles.forEach(tile => tile.style.flexBasis = basis);
+}
+cambiaTamanoDeGrid();
 
 function cambiaColor(e){
 
