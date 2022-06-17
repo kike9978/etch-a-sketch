a71c3a65b096d275716560be6eb89a0ae546619f
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-06-17 13:43:23 -0500

    Adds button to change side size

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,43 @@
 const container = document.querySelector(".container");
 const div = document.createElement("div");
+const btnGridsize = document.querySelector("[data-btn=grid-size]");
 
 
 // Create a modular flex square grid that can change the cuantity of 
 // elements in the grid without changing the size of its container
 
-let lado = 100;
-let tamañoDeGrid = lado * lado;
+let lado = 4;
 
-
-for(let i = 0; i<tamañoDeGrid; i++){
-    const div = document.createElement("div");
-    div.classList = "tile";
-    container.appendChild(div);
+function llenaGrid(){
+    const tamañoDeGrid = lado * lado;
+    for(let i = 0; i<tamañoDeGrid; i++){
+        const div = document.createElement("div");
+        div.classList = "tile";
+        container.appendChild(div);
+    }
+    console.log(tamañoDeGrid);
 }
+llenaGrid();
 
 
-const tiles = document.querySelectorAll(".tile");
+
+let tiles = document.querySelectorAll(".tile");
 tiles.forEach((tile) => tile.addEventListener("mouseover", cambiaColor))
 
+
 function cambiaTamanoDeGrid(){
+    container.innerHTML="";
+    lado = prompt("¿Que longitud quieres para tu lado?", 4);
     console.log(lado);
-    console.log(100/lado);
-    let basis = `${Math.round(100/lado)}%`;
+    const basis = `${Math.round(100/lado)}%`;
+    console.log(`Mi basis es ${basis}`);
+
+    llenaGrid();
+
+    tiles = document.querySelectorAll(".tile");
+    tiles.forEach((tile) => tile.addEventListener("mouseover", cambiaColor))
     tiles.forEach(tile => tile.style.flexBasis = basis);
 }
-cambiaTamanoDeGrid();
 
 function cambiaColor(e){
 
@@ -38,3 +50,4 @@ function giveRandomColor(){
     const randomColor = Math.floor(Math.random() * 255);
     return randomColor;
 }
+btnGridsize.addEventListener("click", cambiaTamanoDeGrid);
