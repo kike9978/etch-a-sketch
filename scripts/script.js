fd37dd7ee8a4c23cb5f48e8f4646b41b91646b3f
Kike Mendoza <kike.mendoza9978@gmail.com>
2022-06-17 21:19:49 -0500

    Creates eraser and darken features

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,25 @@
 const container = document.querySelector(".container");
 const div = document.createElement("div");
-const btnGridsize = document.querySelector("[data-btn=grid-size]");
 const textoLado = document.querySelector("[data-text=lado]");
 const slider = document.querySelector("[data-input=slider]");
 const inputLado = document.querySelector("[data-input=valor-lado]");
 const btnClear = document.querySelector("[data-btn=grid-clear]");
 const inputColor = document.querySelector("[data-input=color]")
+const btnRainbow = document.querySelector("[data-btn=rainbow]")
+const btnEraser = document.querySelector("[data-btn=eraser]");
+const btnDarken = document.querySelector("[data-btn=darken]");
+
 
 
 // Create a modular flex square grid that can change the cuantity of 
 // elements in the grid without changing the size of its container
 
 let lado = slider.value;
-textoLado.textContent=lado;
+textoLado.textContent = lado;
 
 let color = inputColor.value;
-
+let rainbowActive = false;
+let darkenActive = false;
 
 
 
@@ -27,9 +31,23 @@ inputLado.addEventListener("input", () => console.log(inputLado.value));
 
 inputColor.addEventListener("input", escogeColor);
 
-function llenaGrid(){
+btnRainbow.addEventListener("click", () => rainbowActive = true);
+
+btnEraser.addEventListener("click", () => {
+    rainbowActive = false;
+    color = "white"
+});
+
+btnDarken.addEventListener("click", () => {
+    darkenActive = true;
+    rainbowActive = false;
+})
+
+
+
+function llenaGrid() {
     const tamañoDeGrid = lado * lado;
-    for(let i = 0; i<tamañoDeGrid; i++){
+    for (let i = 0; i < tamañoDeGrid; i++) {
         const div = document.createElement("div");
         div.classList = "tile";
         container.appendChild(div);
@@ -45,18 +63,18 @@ tiles.forEach((tile) => tile.addEventListener("mouseover", cambiaColor))
 
 
 
-function cambiaTamanoDeGrid(){
+function cambiaTamanoDeGrid() {
     const currentLado = lado;
     lado = this.value;
-    if (lado === currentLado){
+    if (lado === currentLado) {
         return;
     }
-    container.innerHTML="";
-    const basis = `${Math.floor((100/lado) * 100)/100}%`;
+    container.innerHTML = "";
+    const basis = `${Math.floor((100 / lado) * 100) / 100}%`;
     // const basis = `${parseFloat(100/lado)}%`;
-    textoLado.textContent=lado;
+    textoLado.textContent = lado;
 
-    
+
 
     llenaGrid();
 
@@ -66,26 +84,53 @@ function cambiaTamanoDeGrid(){
     tiles.forEach(tile => tile.style.flexBasis = basis);
 }
 
-function cambiaColor(e){
+function cambiaColor(e) {
 
-    e.target.style.backgroundColor = color;
-    // e.target.style.backgroundColor = `rgb(${giveRandomColor()}, 
-    // ${giveRandomColor()}, ${giveRandomColor()})`;
+    if (!rainbowActive && !darkenActive) {
+        e.target.style.backgroundColor = color;
+
+
+        return;
+    }
+    if (!rainbowActive) {
+        //  Experimentos
+        let divColor = e.target.style.backgroundColor.replace(/[r/g/b/(/)]/g, "");
+        let arrayRapid = divColor.split(",");
+
+        console.log(divColor);
+        console.log(arrayRapid);
+        e.target.style.backgroundColor = `rgb(${arrayRapid[0]-20}, 
+    ${arrayRapid[1] - 20}, ${arrayRapid[2]-20})`;
+
+        return;
+
+        // Acaban experimentos
+    }
+
+    e.target.style.backgroundColor = `rgb(${giveRandomColor()}, 
+    ${giveRandomColor()}, ${giveRandomColor()})`;
+}
+
+
+function darkenColor(e){
+    darkenActive= true;
 }
 
 btnClear.addEventListener("click", () => tiles.forEach((tile) => tile.style.backgroundColor = "#fff"));
 
 
 
-function escogeColor(e){
+function escogeColor(e) {
+    rainbowActive = false;
+    darkenActive = false;
     color = e.target.value;
     console.log(e.target.value);
     return color;
 }
 
 // Creates random rgba color
-function giveRandomColor(){
+function giveRandomColor() {
     const randomColor = Math.floor(Math.random() * 255);
     return randomColor;
 }
-btnGridsize.addEventListener("click", cambiaTamanoDeGrid);
+
